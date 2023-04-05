39efada9c1712e2306487e035a5c42dcc4e0570d
Mike DelGaudio <delgaudiomike@gmail.com>
2023-04-05 10:35:15 -0700

    adding in next app wiht tailwind

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main>
+      <h1 className="text-red-500 text-3xl text-center">Login</h1>
+    </main>
+  );
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <main>
+      <h1 className="text-red-500 text-3xl text-center">Register</h1>
+    </main>
+  );
+}
