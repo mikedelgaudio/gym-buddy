20ded118fa3a2d213615c998c38d683c3f395ebc
Mike DelGaudio <delgaudiomike@gmail.com>
2024-01-23 13:35:11 -0800

    restart

--- app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <main>
-      <h1 className="text-red-500 text-3xl text-center">Login</h1>
-    </main>
-  );
-}
--- app/register/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Register() {
-  return (
-    <main>
-      <h1 className="text-red-500 text-3xl text-center">Register</h1>
-    </main>
-  );
-}
